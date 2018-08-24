635798318f23450c05e0b111968bc9f93205e8b3
HongXiang <hx.hongxiang@gmail.com>
2018-08-24 21:28:40 +0800

    renamed tensor

--- src/python/doufo/tensor/unary_with_args.py
+++ src/python/doufo/tensor/unary_with_args.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 import cntk
 from doufo import singledispatch
 
-__all__ = ['transpose', 'norm', 'flatten']
+__all__ = ['transpose', 'norm', 'flatten', 'one_hot']
 
 
 @singledispatch(nargs=2, nouts=1)
@@ -38,45 +38,47 @@ def _(t, p=2.0):
 
 
 @singledispatch(nargs=2, nouts=1)
-def flatten(x, batch_sim=0):
+def flatten(t, batch_sim=0):
     raise NotImplementedError()
 
 
 @flatten.register(tf.Tensor)
-def _(x, batch_dim=0):
-    return tf.keras.layers.Flatten()(x)
+def _(t, batch_dim=0):
+    return tf.keras.layers.Flatten()(t)
 
 
 @flatten.register(cntk.Variable)
-def _(x, batch_dim=0):
-    return cntk.squeeze(cntk.flatten(x, axis=batch_dim))
+def _(t, batch_dim=0):
+    return cntk.squeeze(cntk.flatten(t, axis=batch_dim))
 
 
 @flatten.register(np.ndarray)
-def _(x, batch_dim=0):
+def _(t, batch_dim=0):
     if batch_dim == 0:
-        return x.reshape([x.shape[0], -1])
+        return t.reshape([t.shape[0], -1])
     raise NotImplementedError(
         "Flatten for numpy Tensor with batch_dim != 0 is not implemented yet")
 
 
+# TODO add sparse support
+
 @singledispatch(nargs=2, nouts=1)
-def one_hot(x, nb_classes):
-    return x.fmap(lambda _: one_hot(_, nb_classes))
+def one_hot(t, nb_classes):
+    return t.fmap(lambda _: one_hot(_, nb_classes))
 
 
 @one_hot.register(np.ndarray)
-def _(x, nb_classes):
-    result = np.zeros(list(x.shape) + [nb_classes])
-    result[np.arange(x.size), x] = 1
+def _(t, nb_classes):
+    result = np.zeros(list(t.shape) + [nb_classes])
+    result[np.arange(t.size), t] = 1
     return result
 
 
 @one_hot.register(cntk.Variable)
-def _(x, nb_classes):
-    return cntk.one_hot(x, nb_classes)
+def _(t, nb_classes):
+    return cntk.one_hot(t, nb_classes)
 
 
 @one_hot.register(tf.Tensor)
-def _(x, nb_classes):
-    return tf.keras.backend.one_hot(x, nb_classes)
+def _(t, nb_classes):
+    return tf.keras.backend.one_hot(t, nb_classes)
