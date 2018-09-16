e87b4447c18a7d17724519569f886f6183ef50a8
Minghao <mh.guo0111@gmail.com>
2018-09-17 00:49:10 +0800

    ADD comments and README.md documents
    1 ADDing comments in the programs without any changes on codes.
    2 Wrote README.md documents

--- src/python/doufo/control.py
+++ src/python/doufo/control.py
@@ -15,6 +15,20 @@ B = TypeVar('B')
 __all__ = ['Functor', 'Monad']
 
 class Functor(Generic[A], metaclass=ABCMeta):
+    """
+    Functor class is the abstract base class of all functors, which defines a set of \
+    functions those satify the properties of functors, who transfer a Category to \
+    another Category:
+    A functor F has to provide the capablitities:
+    1. morphism from A to F[A]
+    2. morphism form f: A->B to F[f]: F[A]->F[B]
+    In fact, the method 'map' is to provide the capablity
+    -----------------------------------------------------------------------
+    A:C
+    F[A]:D
+    f:A->B
+    map(f): F[A]->F[B]
+    """
     @abstractmethod
     def fmap(self, f: Callable[[A], B]) -> 'Functor[B]':
         pass
@@ -26,6 +40,20 @@ class Functor(Generic[A], metaclass=ABCMeta):
 
 class Monad(Functor[A]):
     """
+    A monad is a functor who provide one more capablity, which is bind
+    A monad has to provide:
+    1. morphism from A to M[A]
+    2. morphism form f: A->B to M[f]: M[A]->M[B]
+    3. flatten: M[M[A]] -> M[A]
+    'flatMap' is flatten&Map indeed.
+    -----------------------------------------------------------------------
+    A:C
+    M[A]:D
+    f:A->B
+    map(M): M[A]->M[B]
+    g:A->M[B]
+    flatMap(g):M[A]->M[B]
+    flatten:M[M[A]]->M[A]
     """
     def __rshift__(self, f: Callable[[A], 'Monad[B]']) -> 'Monad[B]':
         """ Alias to bind """
