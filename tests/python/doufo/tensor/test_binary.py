4cc6b57863a5624e093300ff0dfb3ef5d6918b36
hongjiang <hongjiangdong@outlook.com>
2018-09-27 11:07:41 +0800

    Modify the problem in PR

--- tests/python/doufo/tensor/test_binary.py
+++ tests/python/doufo/tensor/test_binary.py
@@ -32,7 +32,7 @@ def test_matmul_SparseTensor_Tensor():
 
     res = matmul(st, t)
     with tf.Session() as sess:
-        assert all_close(sess.run(res) == np.array([[1, 1], [2, 2]]))
+        assert all_close(sess.run(res) , np.array([[1, 1], [2, 2]]))
 
 
 # TODO
