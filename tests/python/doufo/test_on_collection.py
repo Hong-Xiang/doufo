454760cbeaab07007a0349e0d8f3448786f493db
hongjiang <hongjiangdong@outlook.com>
2018-10-17 21:45:41 +0800

    add some tests

--- tests/python/doufo/test_on_collection.py
+++ tests/python/doufo/test_on_collection.py
@@ -1,6 +1,14 @@
 from doufo import head, tail, take, List, flatten, concat
 import pytest
 
+@pytest.mark.skip()
+@pytest.mark.parametrize('n,xs',[
+    (1,[1,2,3]),
+    (2,[1,2,3]),
+    (3,[1,2,3])
+])
+def test_take(n,xs):
+    assert take(n,xs) == [1,2,3][0:n]
 
 @pytest.mark.parametrize('xs,expect', [
     ([1, 2, 3], [2, 3]),
@@ -34,3 +42,12 @@ def test_flatten(xs, expect):
 ])
 def test_concat(xs, expect):
     assert concat(xs, None) == expect
+
+
+import doufo.on_collections
+@pytest.mark.parametrize('xss,op,acc,result',[
+    (lambda x,y:x+y,[1,2],None,3),
+    (lambda x,y:x+y,[2,3],1,6),
+])
+def test_concat_kernel(xss,op,acc,result):
+    assert doufo.on_collections.concat_kernel(op, xss, acc) == result
