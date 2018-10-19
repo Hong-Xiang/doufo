dc94890b139880c9894d8665ab00d46ab4a33424
hongjiang <hongjiangdong@outlook.com>
2018-10-19 21:02:54 +0800

    test on_collection,iterable passed

--- tests/python/doufo/test_iterable.py
+++ tests/python/doufo/test_iterable.py
@@ -1,6 +1,7 @@
 from functools import partial
 from itertools import dropwhile
-from doufo import IterableElemMap, IterableIterMap
+from doufo import IterableElemMap, IterableIterMap,Count
+from doufo.on_collections import take_
 
 
 def test_itertools_iterable():
@@ -12,3 +13,100 @@ def test_chainable_iterable():
     it = range(10)
     it = IterableElemMap(it, lambda x: x + 11)
     assert list(it) + list(it) == list(range(11, 21)) * 2
+
+
+def test_IterableElemMap_init():
+    func = lambda x: x + 1
+    obj = IterableElemMap([1, 2, 3], func)
+    assert obj.source == [1, 2, 3]
+    assert id(obj.operation) == id(func)
+
+
+def test_IterableElemMap_init2():
+    res = IterableElemMap([1, 2, 3])
+    assert res.source == [1, 2, 3]
+    assert res.operation is not None
+
+
+def test_IterableElemMap_fmap():
+    obj = IterableElemMap([1, 2, 3], lambda x: x + 1)
+    obj2 = obj.fmap([2, 3])
+    assert obj2.operation == [2, 3]
+
+
+def test_IterableElemMap_unbox():
+    obj = IterableElemMap([1, 2, 3], lambda x: x + 1)
+    y = 2
+    for x in obj.unbox():
+        assert x == y
+        y += 1
+
+
+def test_IterableElemMap_empty():
+    res = IterableElemMap.empty()
+    assert type(res.source) == tuple
+
+
+# TODO
+def test_IterableElemMap_extend():
+    pass
+
+
+# TODO
+def test_IterableElemMap_filter():
+    pass
+
+
+def test_IterableIterMap_init():
+    func = lambda x: x + 1
+    obj = IterableIterMap([1, 2, 3], func)
+    assert obj.source == [1, 2, 3]
+    assert id(obj.operation) == id(func)
+
+
+def test_IterableIterMap_init2():
+    res = IterableIterMap([1, 2, 3])
+    assert res.source == [1, 2, 3]
+    assert res.operation is not None
+
+
+# TODO（Source code is error）
+def test_IterableIterMap_fmap():
+    pass
+
+
+def test_IterableIterMap_unbox():
+    def func(y):
+        return [x for x in y]
+
+    obj = IterableIterMap([1, 2, 3], func)
+    res = obj.unbox()
+    z = 1
+    for x in res:
+        assert x == z
+        z += 1
+
+
+# TODO（Source code is error）
+def test_IterableIterMap_extend():
+    pass
+
+
+# TODO（Source code is error）
+def test_IterableIterMap_filter():
+    pass
+
+
+def test_empty():
+    res = IterableIterMap.empty()
+    assert type(res.source) == tuple
+
+# def test_Count_init():
+#     count = Count([1],[2])
+#     assert count.start == [1]
+#     assert count.step == [2]
+
+
+#TODO(The abc in on_collections.py:32 does not exist.)
+def test_take_():
+    pass
--- tests/python/doufo/test_on_collection.py
+++ tests/python/doufo/test_on_collection.py
@@ -1,14 +1,15 @@
-from doufo import head, tail, take, List, flatten, concat
+from doufo import *
 import pytest
 
-@pytest.mark.skip()
-@pytest.mark.parametrize('n,xs',[
-    (1,[1,2,3]),
-    (2,[1,2,3]),
-    (3,[1,2,3])
+
+@pytest.mark.parametrize('n,xs', [
+    (1, [1, 2, 3]),
+    (2, [1, 2, 3]),
+    (3, [1, 2, 3])
 ])
-def test_take(n,xs):
-    assert take(n,xs) == [1,2,3][0:n]
+def test_take(n, xs):
+    assert take(n, xs) == [1, 2, 3][0:n]
+
 
 @pytest.mark.parametrize('xs,expect', [
     ([1, 2, 3], [2, 3]),
@@ -30,8 +31,8 @@ def test_head(xs, expect):
     ((1, 2, 3), (1, 2, 3)),
     ((1, (2, 3)), (1, 2, 3)),
     ([1, [3, 4]], [1, 3, 4]),
-    ([1,2], [1,2]),
-    ((2,3), (2,3)),
+    ([1, 2], [1, 2]),
+    ((2, 3), (2, 3)),
 ])
 def test_flatten(xs, expect):
     assert flatten(xs) == expect
@@ -45,9 +46,36 @@ def test_concat(xs, expect):
 
 
 import doufo.on_collections
-@pytest.mark.parametrize('xss,op,acc,result',[
-    (lambda x,y:x+y,[1,2],None,3),
-    (lambda x,y:x+y,[2,3],1,6),
+
+
+@pytest.mark.parametrize('xss,op,acc,result', [
+    (lambda x, y: x + y, [1, 2], None, 3),
+    (lambda x, y: x + y, [2, 3], 1, 6),
 ])
-def test_concat_kernel(xss,op,acc,result):
+def test_concat_kernel(xss, op, acc, result):
     assert doufo.on_collections.concat_kernel(op, xss, acc) == result
+
+
+@pytest.mark.parametrize('xss,res1,res2', [
+    (([1, 2]), (1,), (2,)),
+    (([1, 2], [2, 3]), ([1, 2],), ([2, 3],))
+])
+def test_fzip(xss, res1, res2):
+    list = []
+    for data in zip(xss):
+        list.append(data)
+    assert list[0] == res1
+    assert list[1] == res2
+
+
+@pytest.mark.parametrize('xs,type_,res', [
+    ([1, 2, 3], int, 1),
+    ('', int, 1),
+    ([1, 2, '3'], int, 0)
+])
+def test_all_isinstance(xs, type_, res):
+    assert all_isinstance(xs, type_) == res
+
+
+def test_fmap():
+    assert fmap([1, 2, 3], lambda x: x + 1) == [2, 3, 4]
